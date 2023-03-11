3241667a962d6be352dc21b0c14028202e7c9ac7
Alexander Andonov <a.andonov@infoleven.com>
2023-03-11 11:53:37 +0200

    update

--- Lectures/1.FirstLecture/src/tasks/fourth-task/main-page.jsx
+++ Lectures/1.FirstLecture/src/tasks/fourth-task/main-page.jsx
@@ -3,6 +3,9 @@ import Content from "./components/content";
 import Footer from "./components/footer";
 import Menu from "./components/menu";
 
+    //Създайте div с размери 50x50. И четири бутона „Нагоре“, „Надолу“, „Наляво“, „Надясно“.
+    //При натискане на някой от бутоните div-a се премества.
+
 export default function MainPage (){
     return (
         <div>
